414923c44984e7bf1239e9531771ae994aaf81e7
Joa9cv28 <joaninecordov@gmail.com>
2025-03-20 13:49:55 -0600

    second application guess mini-game

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet } from 'react-native';
 
 function Title({children}) {
   return <Text style={styles.title}>{children}</Text>;
@@ -8,8 +8,8 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: 'open-sans-bold',
     fontSize: 24,
-    fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
     borderWidth: 2,
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, Alert, Text, FlatList} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import Title from '../components/ui/Title';
 import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Card from '../components/ui/Card';
 import InstructionText from '../components/ui/InstructionText';
+import GuessLogItem from '../components/game/GuessLogItem';
 
 function generatedRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max -min)) + min;
@@ -23,12 +25,18 @@ let maxBoundary = 100;
 function GameScreen({userNumber, onGameOver}) {
   const initialGuess = generatedRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  const [guessRounds, setGuessRounds] = useState([initialGuess]);
 
   useEffect(() => {
     if(currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
+
+  useEffect(() => {
+    minBoundary = 1;
+    maxBoundary = 100;
+  }, []);
 
   function nextGuessHandler(direction) {
     //direction => 'lower' or 'greater'
@@ -49,7 +57,10 @@ function GameScreen({userNumber, onGameOver}) {
     }
     const newRndNumber = generatedRandomBetween(minBoundary, maxBoundary, currentGuess);
     setCurrentGuess(newRndNumber);
+    setGuessRounds(prevGuessRoudns => [newRndNumber, ...prevGuessRoudns]);
   }
+
+  const guessRoundListLength = guessRounds.length;
 
   return (
     <View style={styles.screen}>
@@ -60,17 +71,29 @@ function GameScreen({userNumber, onGameOver}) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
-              -
+              <Ionicons name='remove' size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
-              +
+              <Ionicons name="add" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
       </Card>
-      {/* <View>LOG ROUNDS</View> */}
+      <View style={styles.listContainer}>
+        {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
+        <FlatList  //FlatList is a ScrollView that is optimized to work with long lists of items
+          data={guessRounds} 
+          renderItem={(itemData) => 
+            <GuessLogItem 
+              roundNumber={ guessRoundListLength - itemData.index} 
+              guess={itemData.item}
+            />
+          }
+          keyExtractor={(item) => item}
+        />
+      </View>
     </View> 
   );
 }
@@ -91,5 +114,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
   },
 });
