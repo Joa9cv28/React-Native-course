5e0ffdc2b49e8102a8e8fa6b76aee735406a4f6a
Joa9cv28 <joaninecordov@gmail.com>
2025-03-27 15:27:18 -0600

    wip of the meals app

--- App.js
+++ App.js
@@ -1,78 +1,25 @@
-import { useState } from 'react';
-import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import  { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
-import GameOverScreen from './screens/GameOverScreen';
-import Colors from './constants/colors';
+import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [gameIsOver, setGameIsOver] = useState();
-  const [guessRounds, setGuessRounds] = useState(0);
-
-  const [fontsLoaded] = useFonts({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
-  function pickNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber);
-    setGameIsOver(false);
-  }
-  
-  function gameOverHandler(numberOfRounds) {
-    setGameIsOver(true);
-    setGuessRounds(numberOfRounds)
-  }
-
-  function startNewGameHandler() {
-    setUserNumber(null);
-    setGuessRounds(0);
-  }
-
-  let screen = <StartGameScreen onPickedNumber={pickNumberHandler}/>;
-
-  if(userNumber) {
-    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
-  }
-
-  if(gameIsOver && userNumber) {
-    screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>;
-  }
-
   return(
     <>
-      <StatusBar style='light'/>
-      <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
-        <ImageBackground 
-          source={require('./assets/images/dices.jpg')} 
-          resizeMode="cover"
-          style={styles.rootScreen}
-          imageStyle={styles.backgroundImage}
-        >
-          <SafeAreaView style={styles.rootScreen}>
-            {screen}
-          </SafeAreaView>
-        </ImageBackground>
-      </LinearGradient>
+      <StatusBar style='dark'/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
-  ); 
+  );
 }
 
 const styles = StyleSheet.create({
-  rootScreen:{
-    flex: 1,
-  },
-  backgroundImage: {
-    opacity: 0.15,
-  },
+  container: {},
 });
